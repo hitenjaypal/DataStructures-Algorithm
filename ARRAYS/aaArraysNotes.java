d29c7d64c87c96c413fb7c448275ea6e6e8e6e95
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-02-17 23:13:46 +0530

    Add files via upload

---
+++ ARRAYS/aaArraysNotes.java
@@ -0,0 +1,27 @@
+ //Arrays in Java
+
+// In Java, all arrays are dynamically allocated. (discussed below)
+// Arrays may be stored in contiguous memory [consecutive memory locations].
+
+
+// Since arrays are objects in Java, we can find their length using the object property length. 
+// A Java array variable can also be declared like other variables with [] after the data type.
+
+
+// The variables in the array are ordered, and each has an index beginning with 0.
+// Java array can also be used as a static field, a local variable, or a method parameter.
+
+
+
+// Creation of arrays 
+// datatype arrayname []= new datatype [size]
+// int numbers[] ={1,2,3};
+// String  names[] = {"John","Jane", "Tom"};
+
+// Accessing elements of an array:
+// arrayname[index]
+// System.out.println(names[0]);   // Output: John
+
+
+// The new Keyword in array
+// --> New Keyword finds the space to store Array in contiguousway we cannot change the size of array during runtime 
