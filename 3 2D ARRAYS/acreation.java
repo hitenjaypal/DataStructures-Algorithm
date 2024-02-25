d714fb338805b8c3e115cc33636975ba36c36a99
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-02-25 15:48:25 +0530

    Update acreation.java

--- 3 2D ARRAYS/acreation.java
+++ 3 2D ARRAYS/acreation.java
@@ -1,3 +1,32 @@
+import java.util.*;
+
 public class acreation {
-    
+    public static void main(String[] args) {
+        // Creating 2D array
+        int matrix[][] = new int[3][3];
+
+        // Intialized Variables (Row/Col)
+        int n = 3, m = 3; // Both are same int n = matrix.length ,m= matrix[0].length;
+
+        // INPUT 
+        // Now we are taking Inputs from the users for our 2 D Matrix
+        Scanner sc = new Scanner(System.in);
+        // running loops for rows and columns of the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // Accessing the matrix
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // OUTPUT
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+                
+            }
+            System.out.println("");
+        }
+    }
+
 }
