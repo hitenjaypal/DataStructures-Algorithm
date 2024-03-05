db2133eab74aafda026ee317ab60d174e9916329
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-03-05 23:28:21 +0530

    Update acreation.java

--- 4 STRINGS/acreation.java
+++ 4 STRINGS/acreation.java
@@ -6,7 +6,7 @@ public class acreation {
         // Creation of String 
         char arr [] = {'a','b','c','d'};
         String str = "abcd";
-        String str2 = new String ("abcd");
+        String str2 = new String ("abcd");  // You can Write any Character @$#%
 
         // Strings are IMMUTABLE
 
@@ -17,5 +17,9 @@ public class acreation {
         name = sc.nextLine(); // Will print Full string 
 
         System.out.println(name);
+
+
+        String fullName =  "John Doe" ;
+        System.out.println(fullName.length()); //8  // For Finding the Length of String It is similar to array just here is // eExtra () paratheseis  
     }
 }
