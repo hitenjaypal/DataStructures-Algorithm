92656cdca5a5f52b2f18fcce3b4a67cc6b64f867
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-03-05 23:19:06 +0530

    Add files via upload

---
+++ 4 STRINGS/acreation.java
@@ -0,0 +1,10 @@
+public class acreation {
+    public static void main(String[] args) {
+        // Creation of String 
+        char arr [] = {'a','b','c','d'};
+        String str = "abcd";
+        String str2 = new String ("abcd");
+
+        // Strings are IMMUTABLE
+    }
+}
