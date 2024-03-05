ea151d02f4665ed4f9bb8e7ba190b8d16da695ee
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-03-05 23:24:34 +0530

    Update acreation.java

--- 4 STRINGS/acreation.java
+++ 4 STRINGS/acreation.java
@@ -1,3 +1,6 @@
+import java .util.*;
+
+
 public class acreation {
     public static void main(String[] args) {
         // Creation of String 
@@ -6,5 +9,13 @@ public class acreation {
         String str2 = new String ("abcd");
 
         // Strings are IMMUTABLE
+
+
+        Scanner sc = new Scanner (System.in);
+        String name ;   //eg  int name ; 
+        // name = sc.next();  // Will only Print First word 
+        name = sc.nextLine(); // Will print Full string 
+
+        System.out.println(name);
     }
 }
